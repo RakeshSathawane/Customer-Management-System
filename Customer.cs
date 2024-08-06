9ff41902815a46d61dedbab6e97cdece474cb286
Rakesh Sathawane <rakeshsathawane89@gmail.com>
2024-08-06 12:54:29 +0530

    Customer Class Created by Rakesh

---
+++ Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+		public int CustomerID { get; set; }
+	    public string CustomerName { get; set; }
+	    public string CustomerAddress { get; set; }
+	    public string CustomerCity { get; set; }
+
+
+	}
+}
